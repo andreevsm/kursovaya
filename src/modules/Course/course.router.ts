91dc7a43d1acce7d3c61aff028980c9159e992ff
Сергей Андреев <sergey-andreev-97-97@mail.ru>
2020-03-29 03:28:05 +0300

    Code refactoring of Course

--- src/modules/Course/course.router.ts
+++ src/modules/Course/course.router.ts
@@ -8,12 +8,9 @@ export const courseRouter = new Router({
 
 courseRouter
   .get('/', async (ctx: IRouterContext) => {
-    console.log('ctx', ctx);
     const courses = await Course.find();
     ctx.body = courses;
-  });
-
-courseRouter
+  })
   .post('/', async (ctx: IRouterContext) => {
     await Course.create(ctx.request.body);
   });
