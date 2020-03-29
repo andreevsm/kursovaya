4199a3f745e1a4bf9fb843b6a40a83190679034c
Сергей Андреев <sergey-andreev-97-97@mail.ru>
2020-03-29 03:27:22 +0300

    Added influxDB

--- src/handlers/influx-db.ts
+++ src/handlers/influx-db.ts
@@ -5,24 +5,24 @@ import { hostname } from 'os';
 import { influx } from '../lib';
 
 export const initInfluxDB = (app: Koa) => app.use(async (ctx: IRouterContext, next: any) => {
-  // const startDate = Date.now();
+  const startDate = Date.now();
 
-  // ctx.res.on('finish', async () => {
-  //   const duration = +(Date.now() - startDate);
-  //   console.log(`Request to ${ctx.request.path} took ${duration}ms`)
+  ctx.res.on('finish', async () => {
+    const duration = Date.now() - startDate;
+    console.log(`Request to ${ctx.request.path} took ${duration}ms`)
 
-  //   try {
-  //     await influx.writePoints([
-  //       {
-  //         measurement: 'response_timestamps',
-  //         tags: { host: hostname() },
-  //         fields: { duration, path: ctx.request.path }
-  //       }
-  //     ])
-  //   } catch (error) {
-  //     console.error(error.message);
-  //   }
-  // })
+    try {
+      await influx.writePoints([
+        {
+          measurement: 'response_timestamps',
+          tags: { host: hostname() },
+          fields: { duration, path: ctx.request.path }
+        }
+      ])
+    } catch (error) {
+      console.error(error.message);
+    }
+  })
 
-  next();
+  await next();
 })
--- src/modules/ResponseTimestamps/response-timestamps.ts
+++ src/modules/ResponseTimestamps/response-timestamps.ts
@@ -3,24 +3,20 @@ import Router, { IRouterContext } from "koa-router";
 import { influx } from '../../lib';
 
 export const responseTimestampsRouter = new Router({
-  prefix: 'response-timestamps'
+  prefix: '/response-timestamps'
 });
 
 responseTimestampsRouter
   .get('/', async (ctx: IRouterContext) => {
+    try {
+      const responseTimestamps = await influx.query(`
+        select * from response_timestamps
+        order by time desc
+        limit 10
+      `);
 
-    // let responseTimestamps;
-
-    // try {
-    //   responseTimestamps = await influx.query(`
-    //     select * from response_timestamps
-    //     order by time desc
-    //     limit 10
-    //   `);
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-    ctx.body = new Date();
-
+      ctx.body = responseTimestamps;
+    } catch (error) {
+      console.error(error);
+    }
   })
